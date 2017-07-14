3f2c25b8b5bea6e40f70d8653a3a9d9dd5368fc2
Han <allen0346@gmail.com>
2017-07-14 16:39:20 +0800

    [Update] parallax scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,45 @@
+import ContentWrapper from './common-utils/section-content-wrapper'
 import LogoDesktop from '../static/logo-desktop.svg'
 import LogoMobile from '../static/logo-mobile.svg'
 import React from 'react'
 import styled from 'styled-components'
 import SearchBox from './search-box'
 import TRLink from './common-utils/twreporter-link'
+// import Waypoint from 'react-waypoint'
 import { headerPadding } from '../styles/common-variables'
-import ContentWrapper from './common-utils/section-content-wrapper'
+import { breakPoints, finalMedia } from '../utils/style-utils'
 
-const tabletWidth = '916px'
-const mobileWidth = '550px'
+const mobileWidth = breakPoints.mobileMaxWidth
+
+const Container = styled.div`
+  width: 100%;
+  background-color: white;
+  ${(props) => {
+    if (props.ifPinned) {
+      return `
+        position: absolute;
+        bottom: 0;
+      `
+    }
+    return `
+      position: fixed;
+      top: 0;
+    `
+  }};
+  z-index: 2;
+`
 
 const ContentContainer = ContentWrapper.extend`
   height: 62px;
   padding: 0 ${headerPadding.desktop} 0 ${headerPadding.desktop};
-  @media (max-width: ${tabletWidth}) {
+
+  ${finalMedia.tablet`
     padding: 0 ${headerPadding.tablet} 0 ${headerPadding.tablet};
-  }
-  @media (max-width: ${mobileWidth}) {
+  `}
+
+  ${finalMedia.mobile`
     padding: 0 ${headerPadding.mobile} 0 ${headerPadding.mobile};
-  }
+  `}
 `
 
 const LogoFrame = styled.div`
@@ -44,22 +65,52 @@ const LogoMobileContainer = styled.div`
   }
 `
 
-const Header = () => {
-  return (
-    <ContentContainer>
-      <LogoFrame>
-        <TRLink href={''}>
-          <LogoDesktopContainer>
-            <LogoDesktop width="220px" />
-          </LogoDesktopContainer>
-          <LogoMobileContainer>
-            <LogoMobile width="119px" />
-          </LogoMobileContainer>
-        </TRLink>
-      </LogoFrame>
-      <SearchBox />
-    </ContentContainer>
-  )
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ifPinned: false,
+    }
+    this.headerSectionHeight = 278
+    this.handleScroll = this._handleScroll.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+    this.headerSectionHeight = document.getElementById('headerSectionHeight').offsetHeight
+  }
+
+  _handleScroll(e) {
+    if (e.srcElement.body.scrollTop >= this.headerSectionHeight) {
+      this.setState({
+        ifPinned: true,
+      })
+    } else {
+      this.setState({
+        ifPinned: false,
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Container ifPinned={this.state.ifPinned}>
+        <ContentContainer>
+          <LogoFrame>
+            <TRLink href={''}>
+              <LogoDesktopContainer>
+                <LogoDesktop width="220px" />
+              </LogoDesktopContainer>
+              <LogoMobileContainer>
+                <LogoMobile width="119px" />
+              </LogoMobileContainer>
+            </TRLink>
+          </LogoFrame>
+          <SearchBox />
+        </ContentContainer>
+      </Container>
+    )
+  }
 }
 
 export default Header

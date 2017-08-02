12ba4172e62ca0c4ec98cd99d7f12127bcf82494
nickhsine <nickhsine97753017@gmail.com>
2017-08-02 14:39:53 +0800

    Several updates 1. Update gulpfile.js 2. Update side bar strings(本月專題 -> 最新專題, 贊助我們 -> 關於我們) 3. Delete src/conf/api-config.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 /* eslint-disable */
-var gulp = require("gulp")
-var babel = require("gulp-babel")
+var babel = require('gulp-babel')
+var gulp = require('gulp')
+var path = require('path')
 
+var dest = './lib'
 var theBase = { base: './src' }
 
-var dest = 'lib'
-
-function copy() {
+// This function will copy the transpiled files to
+// the customer folder. You can specify the customer folder path
+// by running gulp command  like
+// `CUSTOMER_FOLDER=/home/nick/codes/twreporter-react gulp run dev`,
+// and those transpiled files will be copyed into
+// `/home/nick/codes/twreporter-react/node_modules/twreporter-react-index-page-components/lib`
+function copyToCustomerFolder() {
+  let customerFolder = process.env.CUSTOMER_FOLDER
+  if (typeof customerFolder !== 'string') {
+    customerFolder = path.resolve(__dirname + '/../twreporter-react')
+  }
   return gulp
-    .src('./src/**/*')
-    .pipe(gulp.dest(dest))
+    .src('./lib/**/*')
+    .pipe(gulp.dest(customerFolder+'/node_modules/twreporter-react-index-page-components/lib'))
 }
 
 function transpile() {
   return gulp.src('./src/**/*.js', theBase)
-    .pipe(babel({
-        presets: ['stage-0']
-    }))
+    .pipe(babel())
     .pipe(gulp.dest(dest))
 }
 
@@ -24,6 +32,11 @@ function watch() {
   gulp.watch('./src/**/*.js', transpile)
 }
 
-var build = gulp.series(copy, transpile, watch)
+gulp.task('build', transpile)
 
-gulp.task('default', build)
+gulp.task('dev', () => {
+  const watcher = gulp.watch(['src/**'], gulp.series('build', copyToCustomerFolder))
+  watcher.on('change', (filePath) => {
+    console.log(`File ${filePath} was changed, running tasks...`)
+  })
+})
